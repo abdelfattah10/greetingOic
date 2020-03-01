9b8968782f48028a933b48838540bfcc224f0338
Mahmoud Abdelfattah <abdelfattah10@yahoo.com>
2020-03-01 17:20:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/greeting/:name', function(req, res){
+  res.json({
+    resp: 'Hello '+req.params.name,
+    country: 'World'
+  })
+})
+
+app.listen(3000);
